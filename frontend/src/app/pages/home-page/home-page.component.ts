9d9ae0cf09303c6425915fd6586d505ce9bbb57e
robertzach66 <robert.zach@live.de>
2024-09-29 21:14:36 +0200

    order and product

--- frontend/src/app/pages/home-page/home-page.component.ts
+++ frontend/src/app/pages/home-page/home-page.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { AsyncPipe, JsonPipe } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { OidcSecurityService } from 'angular-auth-oidc-client';
+import { OrderService } from '../../services/order.service';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../model/product';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-home-page',
+  selector: 'app-homepage',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, JsonPipe, FormsModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
+  private readonly oidcSecurityService = inject(OidcSecurityService);
+  private readonly productService = inject(ProductService);
+  private readonly orderService = inject(OrderService);
+  private readonly router = inject(Router);
+
+  isAuthenticated = false;
+  orderSucces = false;
+  orderFailed = false;
+  quantityIsNull = false;
+
+  products: Array<Product> = [];
+
+  ngOnInit(): void {
+    this.oidcSecurityService.isAuthenticated$.subscribe(
+      ({isAuthenticated}) => {
+        this.isAuthenticated = isAuthenticated;
+        this.productService
+          .getProducts()
+          .pipe()
+          .subscribe(products => this.products)
+      }
+    )
+  }
+
+  goToCreateProductPage() {
+    this.router.navigateByUrl('/add-product');
+  }
 
 }
--- frontend/src/app/services/product.service.ts
+++ frontend/src/app/services/product.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../model/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getProducts(): Observable<Array<Product>> {
+    return this.httpClient.get<Array<Product>>("http://localhost:9000/api/product");
+  }
 }
